8f12cc8377a2197348a506611dc11ee9da25a8de
AayushMohan <aayushmohan1702@gmail.com>
2023-05-06 20:35:56 +0530

    Added gradient effect

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,17 @@ import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <main className="">
-      <Card>
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center max-w-3xl">
+      <Card className="max-w-4xl mx-auto">
         <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
         <Subtitle className="text-xl text-center">
           Powered by Next.js 13.3, Tailwind CSS, Tremor 2.0 + More!
         </Subtitle>
+        <Divider className="my-10" />
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          {/* City Picker */}
+        </Card>
       </Card>
-    </main>
+    </div>
   );
 }

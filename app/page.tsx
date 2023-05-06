0214a4daca432ab83a0074aae89084b7e8574d9e
AayushMohan <aayushmohan1702@gmail.com>
2023-05-06 20:38:08 +0530

    Added CityPicker Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import CityPicker from "@/components/CityPicker";
 import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
         </Subtitle>
         <Divider className="my-10" />
         <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
-          {/* City Picker */}
+          <CityPicker />
         </Card>
       </Card>
     </div>
